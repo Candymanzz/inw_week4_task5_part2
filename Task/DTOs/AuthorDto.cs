11e353dd0b47fa1bbc7c7a04b5b94ebf8b501f91
Pavel_Stykachov <111525871+Candymanzz@users.noreply.github.com>
2025-10-27 01:21:37 +0300

    feat: add dtos

---
+++ Task/DTOs/AuthorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Task.DTOs
+{
+    public class AuthorDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateOnly DateOfBirth { get; set; }
+        public int BookCount { get; set; }
+    }
+}
---
+++ Task/DTOs/BookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Task.DTOs
+{
+    public class BookDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int PublishedYear { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+    }
+}
